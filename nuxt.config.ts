3a7c39bd2d930b9b1b33b59d71817816ed2e6422
Kai17-a <k.s25skt@gmail.com>
2024-06-15 15:43:34 +0900

    add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: "src/",
   ssr: false,
   devServer: {
     port: 1420
-  }
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_opetions, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
