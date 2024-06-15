e5e6d766b12a8d34d754088ae61609f231f2d1b1
Kai17-a <k.s25skt@gmail.com>
2024-06-15 15:14:19 +0900

    add nuxt.js

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  srcDir: "src/",
+  ssr: false,
+  devServer: {
+    port: 1420
+  }
+})
